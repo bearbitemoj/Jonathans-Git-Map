f90004603bb8362c650ed4f7c84f715740560fa0
USER\Jonathan <bearbite.nirom@gmail.com>
2016-11-14 19:41:53 +0100

    Lab 2 Klar!

--- TDDE01/TDDE01 Lab 2.2.R
+++ TDDE01/TDDE01 Lab 2.2.R
@@ -6,7 +6,7 @@ data = read.csv2('tecator.csv')
 Moisture = data$Moisture
 Protein = data$Protein
 
-plot(Moisture,Protein)
+plot(Protein,Moisture)
 
 ############
 ##Step 2-3##
@@ -28,8 +28,6 @@ valid = data[id2,]
 N = 6
 fmla = vector(length=N)
 
-fit1 = vector(length=6)
-
 for(i in 1:N){
   xnam <- paste0("I(Protein^", 1:i,")")
   fmla[i] <- (paste("Moisture ~ ", paste(xnam, collapse= "+")))
@@ -40,24 +38,61 @@ mseMatrix = matrix(nrow=N,ncol=2)
 for(i in 1:N){
   fit1 = lm(formula(fmla[i]),data = train)
   sumfit = summary(fit1)
-
   mseMatrix[i,1] = mse(sumfit)
-}
-
-for(i in 1:N){
-  fit1 = lm(formula(fmla[i]),data = valid)
-  sumfit = summary(fit1)
   
-  mseMatrix[i,2] = mse(sumfit)
+  pred = predict(fit1,valid)
+  mseMatrix[i,2] = mean((valid$Moisture-pred)^2)
 }
 
-plot(seq(1,6,1),mseMatrix[,1], type="l",col="blue",xlab="Mi",ylab="Validation Data",ylim=c(30.5,34))
-plot(seq(1,6,1),mseMatrix[,2], type="l",col="red", xlab="Mi",ylab="Test Data",ylim=c(30.5,34))
+
+
+plot(seq(1,N,1),mseMatrix[,1], type="l",col="blue",xlab="Mi",ylab="MSE",ylim=c(31,35))
+par(new=TRUE)
+plot(seq(1,N,1),mseMatrix[,2], type="l",col="red", xlab="Mi",ylab="MSE",ylim=c(31,35))
 
 
 ############
 ###Step 4###
 ############
+N = 100
+
+myvars <- names(data) %in% c("Sample","Fat","Protein","Moisture") 
+newdata <- data[!myvars]
+xnam <- paste0("I(Channel", 1:N,")")
+fmla2 <- (paste("Fat ~ ", paste(xnam, collapse= "+")))
+
+fit2 = lm(as.formula(fmla2),data=data)
+library(MASS)
+step = stepAIC(fit2,direction="both") #63 variables
+
+############
+###Step 5###
+############
+X = as.matrix(newdata)
+Y = as.matrix(data$Fat)
+
+library(glmnet)
+ridge = glmnet(X,Y,alpha=0,family="gaussian")
+plot(ridge,xvar="lambda",label=TRUE)
+
+############
+###Step 6###
+############
+lasso = glmnet(X,Y,alpha=1,family="gaussian")
+plot(lasso,xvar="lambda",label=TRUE)
+
+
+############
+###Step 7###
+############
+model=cv.glmnet(X,Y, alpha=1,family="gaussian")
+model$lambda.min
+plot(model)
+coef(model, s="lambda.min")
+
+
+
+
 
 
 
--- TDDE01/TDDE01 Lab 2.R
+++ TDDE01/TDDE01 Lab 2.R
@@ -1,3 +1,76 @@
 
+############
+###Step 1###
+############
+#linear regression
+mylin=function(X,Y,Xpred){
+  Xpred1=cbind(1,Xpred)
+  X = cbind(1,X)
+  #MISSING: check formulas for linear regression and compute beta
+  beta = solve(t(X)%*%X)%*%t(X)%*%Y
+  Res=Xpred1%*%beta
+  return(Res)
+}
 
-data = read.csv2('spambase.csv')
+myCV=function(X,Y,Nfolds){
+  n=length(Y)
+  p=ncol(X)
+  set.seed(12345)
+  ind=sample(n,n)    #Permute the observations randomly
+  X1=X[ind,]
+  Y1=Y[ind]
+  #sF=floor(n/Nfolds)
+  MSE=numeric(2^p-1)
+  Nfeat=numeric(2^p-1)
+  Features=list()
+  curr=0
+  
+  #we assume 5 features.
+  
+  for (f1 in 0:1)
+    for (f2 in 0:1)
+      for(f3 in 0:1)
+        for(f4 in 0:1)
+          for(f5 in 0:1){
+            model= c(f1,f2,f3,f4,f5)
+            if (sum(model)==0) next()
+            SSE=0
+            
+            folds = cut(seq(1,nrow(X1)),breaks=Nfolds,labels=FALSE)
+            for (k in 1:Nfolds){
+              #MISSING: compute which indices should belong to current fold
+              testIndexes = which(folds==k,arr.ind=TRUE)
+              testDataX = X1[testIndexes, ]
+              testDataY = Y1[testIndexes]
+              trainDataX = X1[-testIndexes, ]
+              trainDataY = Y1[-testIndexes]
+              
+              #MISSING: implement cross-validation for model with features in "model" and iteration i.
+              index = seq(1:5)*model
+              testDataX = as.matrix(testDataX[,index])
+              trainDataX = as.matrix(trainDataX[,index])
+             
+              #MISSING: Get the predicted values for fold 'k', Ypred, and the original values for fold 'k', Yp.
+              Ypred = mylin(trainDataX,trainDataY,testDataX)
+              Yp = testDataY
+              SSE=SSE+sum((Ypred-Yp)^2)
+            }
+            curr=curr+1
+            MSE[curr]=SSE/n
+            Nfeat[curr]=sum(model)
+            Features[[curr]]=model
+            
+          }
+  
+  #MISSING: plot MSE against number of features
+  plot(Nfeat,MSE)
+  i=which.min(MSE)
+  return(list(CV=MSE[i], Features=Features[[i]]))
+  #return(X1)
+}
+
+############
+###Step 2###
+############
+cross = myCV(as.matrix(swiss[,2:6]), swiss[[1]], 5) #Sends in X, Y and Nfolds
+
