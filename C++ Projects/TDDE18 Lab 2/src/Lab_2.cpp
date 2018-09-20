72347821928741a3154773dff0690fbcdc287bad
Jonathan Sjölund <jonathan_sjolund@hotmail.com>
2018-09-20 13:45:31 +0200

    C++ grejs

---
+++ C++ Projects/TDDE18 Lab 2/src/Lab_2.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+#include "Lab_2.h"
+
+//Constructor
+Lab_2::Lab_2(){
+
+}
+
+void Lab_2::main(){
+
+}
---
+++ C++ Projects/TDDE18 Lab 2/src/Lab_2.h
@@ -0,0 +1,14 @@
+
+#ifndef LAB_2_H_INCLUDED
+#define LAB_2_H_INCLUDED
+
+class Lab_2{
+
+//member functions go *inside* the class declaration
+public:
+	Lab_2();
+	void main();
+
+};
+
+#endif
